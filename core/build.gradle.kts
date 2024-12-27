50a59935bdc00e00eeb1e7574c7ea1a54c08ad95
pa.sheludyakov <pa.sheludyakov@tensor.ru>
2024-12-28 02:36:25 +0300

    init commit

---
+++ core/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    alias(libs.plugins.multiplatform)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlinx.serialization)
+    id("convention.publication")
+}
+
+group = "ru.shelldev.auth_module.core"
+version = "1.0"
+
+kotlin {
+    jvmToolchain(11)
+    androidTarget {
+        publishLibraryVariants("release")
+    }
+
+    jvm()
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "core"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.serialization.json)
+        }
+
+        androidMain.dependencies {
+
+        }
+
+        jvmMain.dependencies {
+
+        }
+
+        iosMain.dependencies {
+
+        }
+
+    }
+
+    //https://kotlinlang.org/docs/native-objc-interop.html#export-of-kdoc-comments-to-generated-objective-c-headers
+    targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget> {
+        compilations["main"].compilerOptions.options.freeCompilerArgs.add("-Xexport-kdoc")
+    }
+
+}
+
+android {
+    namespace = "ru.shelldev.auth_module.core"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+}
